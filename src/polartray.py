7d38f533cd12814c9d0b5c30ed8206c519641c50
crito <crito@fnordpipe.org>
2017-06-09 00:08:28 +0200

    add skeleton for device scan thread

--- src/polartray.py
+++ src/polartray.py
@@ -4,17 +4,18 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
+import threading
+import time
+
 class PolarTray(Gtk.StatusIcon):
   def __init__(self):
     Gtk.StatusIcon.__init__(self)
     self.set_from_icon_name('help-about')
     self.set_has_tooltip(True)
     self.set_visible(True)
-    self.connect("popup_menu", self.on_secondary_click)
+    self.connect("popup_menu", self._popup)
 
-    Gtk.main()
-
-  def on_secondary_click(self, widget, button, time):
+  def _popup(self, widget, button, time):
     menu = Gtk.Menu()
 
     menu_quit = Gtk.MenuItem("Quit")
@@ -24,8 +25,21 @@ class PolarTray(Gtk.StatusIcon):
     menu.show_all()
     menu.popup(None, None, None, self, 3, time)
 
+  def _scanDevices(self):
+    self.scanning = True
+    while self.scanning:
+      print('scanning')
+      time.sleep(0.2)
+
+  def run(self):
+    self.threadDeviceScan = threading.Thread(target = self._scanDevices)
+    self.threadDeviceScan.daemon = True
+    self.threadDeviceScan.start()
+
+    Gtk.main()
+
 if __name__ == '__main__':
   try:
-    PolarTray()
+    PolarTray().run()
   except KeyboardInterrupt:
     print('Interrupted')
